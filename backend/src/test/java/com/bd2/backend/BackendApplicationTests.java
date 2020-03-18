ce1fb9f586363c2129ed95b2579564469cd590a2
Pawel Nowak <p.n98@interia.pl>
2020-03-18 23:34:13 +0100

    Created backend using spring initializr

---
+++ backend/src/test/java/com/bd2/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd2.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
