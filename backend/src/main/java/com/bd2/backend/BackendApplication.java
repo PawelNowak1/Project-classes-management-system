78b63d692a0b07badbb1c4663646c09eefcc88ba
Piotr Matras <piotrmatras21@gmail.com>
2020-03-28 15:22:54 +0100

    added bean of BCryptPasswordEncoder to application

--- backend/src/main/java/com/bd2/backend/BackendApplication.java
+++ backend/src/main/java/com/bd2/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package com.bd2.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@ComponentScan("com.bd2.backend.*")
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
