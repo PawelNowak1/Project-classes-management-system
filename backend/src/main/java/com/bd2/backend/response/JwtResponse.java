2d399a13bfec3664fb07c5c16350e3cf6736bb71
Piotr Matras <piotrmatras21@gmail.com>
2020-03-29 18:41:21 +0200

    added new dependencies and class for wrapping response with JWT token

---
+++ backend/src/main/java/com/bd2/backend/response/JwtResponse.java
@@ -0,0 +1,66 @@
+package com.bd2.backend.response;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+
+    public JwtResponse(String token, Long id, String username, String email, String role) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
