802ec7dec90f341b616e7230a55d392edeffe36b
Piotr Matras <piotrmatras21@gmail.com>
2020-03-29 18:39:33 +0200

    reimplemented signing in using /login endpoint
    now after successful login user will receive secret JWT token in response

---
+++ backend/src/main/java/com/bd2/backend/rest/LoginController.java
@@ -0,0 +1,63 @@
+package com.bd2.backend.rest;
+
+import com.bd2.backend.repository.RoleRepository;
+import com.bd2.backend.repository.UserRepository;
+import com.bd2.backend.request.LoginRequest;
+import com.bd2.backend.response.JwtResponse;
+import com.bd2.backend.security.JwtUtils;
+import com.bd2.backend.service.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(element -> element.getAuthority()).collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(
+                jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles.get(0)
+        ));
+    }
+}
