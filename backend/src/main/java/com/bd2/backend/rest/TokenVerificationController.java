05149e63ad4fce0b2a7da142d6f65e0c8f3f7e72
Piotr Matras <piotrmatras21@gmail.com>
2020-05-12 14:35:28 +0200

    added sending response with all details about user associated with token
    also added protection against verifying token which belongs to deleted user

--- backend/src/main/java/com/bd2/backend/rest/TokenVerificationController.java
+++ backend/src/main/java/com/bd2/backend/rest/TokenVerificationController.java
@@ -1,31 +1,55 @@
 package com.bd2.backend.rest;
 
+import com.bd2.backend.entities.User;
+import com.bd2.backend.repository.StudentRepository;
+import com.bd2.backend.repository.TeacherRepository;
+import com.bd2.backend.repository.UserRepository;
 import com.bd2.backend.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 @RequestMapping("/token")
 public class TokenVerificationController {
 
-    @Autowired
     private JwtUtils jwtUtils;
+    private UserRepository userRepository;
+    private TeacherRepository teacherRepository;
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public TokenVerificationController(JwtUtils jwtUtils, UserRepository userRepository,
+                                       TeacherRepository teacherRepository, StudentRepository studentRepository) {
+        this.jwtUtils = jwtUtils;
+        this.userRepository = userRepository;
+        this.teacherRepository = teacherRepository;
+        this.studentRepository = studentRepository;
+    }
 
     @GetMapping("/verify/{token}")
     public ResponseEntity<?> verifyToken(@PathVariable("token") String token) {
         if (jwtUtils.validateJwtToken(token)) {
-            Map<String, String> response = new LinkedHashMap<>();
-            response.put("username", jwtUtils.getUsernameFromJwtToken(token));
-            response.put("valid", "yes");
-            return ResponseEntity.ok(response);
+            User user = this.userRepository.findByUsername(jwtUtils.getUsernameFromJwtToken(token));
+            if (user != null) {
+                switch (user.getRole().getRole()) {
+                    case ROLE_TEACHER:
+                        return ResponseEntity.ok(this.teacherRepository.findById(user.getId()).get());
+                    case ROLE_STUDENT:
+                        return ResponseEntity.ok(this.studentRepository.findById(user.getId()).get());
+                    case ROLE_ADMIN:
+                        return ResponseEntity.ok(user);
+                }
+            }
+            return ResponseEntity.badRequest()
+                    .body("Token is valid, but user associated with token doesn't exist!\n");
         } else {
             return ResponseEntity.ok(Collections.singletonMap("valid", "no"));
         }
