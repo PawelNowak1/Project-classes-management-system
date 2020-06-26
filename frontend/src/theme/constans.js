8cb5f09daf3ac26b9f146bc433fca0c7536fb04f
Adam Wolny <krupowieswojciech@gmail.com>
2020-06-26 16:54:27 +0200

    no message

--- frontend/src/theme/constans.js
+++ frontend/src/theme/constans.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://sekcjeprojektowe.herokuapp.com';
-// export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'https://sekcjeprojektowe.herokuapp.com';
+export const API_URL = 'http://localhost:8080';
 
 export const ONLY_NUMBERS_REGEX = new RegExp(/^\d+$/);
